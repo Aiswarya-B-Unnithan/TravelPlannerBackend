0ade445d65c6b8def729ae0de9f2fbc8ca8c1ae1
Aiswarya-B-Unnithan <140697168+Aiswarya-B-Unnithan@users.noreply.github.com>
2024-01-24 01:11:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   socket.on("add-user", (userId, chatId) => {
-    console.log("newuser is added to socket", userId);
+     alert("newuser is added to socket: " + userId);
     socket.join(chatId);
     const soketId = socket?.id;
     onlineUsers.set(userId, soketId);
@@ -72,7 +72,7 @@ io.on("connection", (socket) => {
     io.emit("update-online-status", onlineUsersArray);
     io.emit("getOnlineUsers", onlineUsersArray);
     io.emit("socket-setup", socket.id);
-    console.log("onlineusersfrom serrver", onlineUsersArray);
+    alert("onlineusersfrom serrver: " + onlineUsersArray);
 
     //--------------------------------------------------->
     socket.on("send-msg", (data) => {
