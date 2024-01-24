f6c20922585c0325905c1cc86a34907ccd74e54a
Aiswarya-B-Unnithan <140697168+Aiswarya-B-Unnithan@users.noreply.github.com>
2024-01-24 10:36:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ dbConnection();
 
 app.use(helmet());
 
-
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use("/public", express.static(path.join(__dirname, "public")));
@@ -64,7 +63,7 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   socket.on("add-user", (userId, chatId) => {
-     alert("newuser is added to socket: " + userId);
+   
     socket.join(chatId);
     const soketId = socket?.id;
     onlineUsers.set(userId, soketId);
@@ -73,7 +72,7 @@ io.on("connection", (socket) => {
     io.emit("update-online-status", onlineUsersArray);
     io.emit("getOnlineUsers", onlineUsersArray);
     io.emit("socket-setup", socket.id);
-    alert("onlineusersfrom serrver: " + onlineUsersArray);
+   
 
     //--------------------------------------------------->
     socket.on("send-msg", (data) => {
