a4f424c04f3221d186e955a1db182ade7eaec8a6
Aiswarya-B-Unnithan <140697168+Aiswarya-B-Unnithan@users.noreply.github.com>
2024-01-24 09:38:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve(path.dirname(""));
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.static(path.join(__dirname, "views/build")));
 
 const PORT = process.env.PORT || 3000;
@@ -25,11 +25,12 @@ const PORT = process.env.PORT || 3000;
 dbConnection();
 
 app.use(helmet());
-app.use(cors());
+
 
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use("/public", express.static(path.join(__dirname, "public")));
+
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
@@ -56,8 +57,10 @@ const server = app.listen(PORT);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://travelplanneronline.netlify.app",
+    origin: "*",
+    methods: ["GET", "POST"],
     credentials: true,
+    allowedHeaders: ["Content-Type"],
   },
 });
 
